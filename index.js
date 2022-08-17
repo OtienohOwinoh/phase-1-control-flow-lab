7e7c00cd75e71f042b1cd19b9e07e8ab67b59e6a
OtienohOwinoh <coowinoh@gmail.com>
2022-08-17 18:11:24 +0300

    COmplete Assignemnt

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+
+  if (ride <= 400){
+    return 'This one is on me!'
+  }
+
+  else if 
+  (ride > 2000 && ride <= 2500) {
+      return 'I will gladly take your thirty bucks.'
+    }
+  
+    else if (ride >= 2500){
+      return 'No can do.'
+    }
+
+
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity( city){
   // Write your code here!
+  const destination = city === "NYC" ? "Ok, sounds good." : "No go.";
+  return destination
+
 }
 
-function switchOnCharmFromTip(){
+ternaryCheckCity("NYC")
+
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case "generous" :
+       return "Thank you so much." ;
+
+       case "not as generous" :
+        return "Thank you."
+
+        default : 
+        return "Bye."
+  }
+
+
   // Write your code here!
+
+
 }
